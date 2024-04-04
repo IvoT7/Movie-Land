409102cb889b29bfa9555603284a7fd092c39d57
Ivo Sardzovski-Teovski <ivosardzovski@hotmail.com>
2024-04-05 01:30:41 +0200

    ss

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -221,3 +221,17 @@ $(document).ready(function(){
         
   fetchQuote();
 });
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 20){
+      $('#scrollTopBtn').fadeIn();
+    } else{
+      $('#scrollTopBtn').fadeOut();
+    }
+  });
+
+  $('#scrollTopBtn').click(function(){
+    $('html, body').animate({scrollTop: 0}, 'slow');
+  });
+});
